4641f400733f8c6094cae33130a8b341fdedcb50
Gnomeslayer <Declan.otten@live.com.au>
2022-12-14 02:19:20 +1100

    Update index.php

--- Project 1/index.php
+++ Project 1/index.php
@@ -3,18 +3,35 @@ include "conn.php";
 include "action_page.php";
 
 
-if (isset($_GET['pageno'])) {
-    $pageno = $_GET['pageno'];
+$studentcount = GetStudentCount($conn);
+
+if (isset($_GET['limit'])) {
+
+    $limit = sanitize_input($_GET['limit']);
 } else {
-    $pageno = 1;
+    $limit = 5;
 }
 
-$nextpage = $page + 1;
-$no_of_records_per_page = 10;
-$offset = ($pageno - 1) * $no_of_records_per_page;
+
+$pages = ceil($studentcount / $limit);
+
+// What page are we currently on?
+$page = min($pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array(
+    'options' => array(
+        'default'   => 1,
+        'min_range' => 1,
+    ),
+)));
+
+// Calculate the offset for the query
+$offset = ($page - 1)  * $limit;
+$start = $offset + 1;
+$end = min(($offset + $limit), $studentcount);
 
 
-$results = GetAllStudents($conn, $start, $end);
+
+
+$results = GetAllStudents($conn, $limit, $offset);
 ?>
 
 
@@ -25,6 +42,14 @@ $results = GetAllStudents($conn, $start, $end);
 <body>
 
     <?php if (sizeof(($results)) > 0) : ?>
+        <?php if ($page > 1) : ?>
+            <a href="?page=1&limit=<?php echo $limit; ?>" title="First page">Start</a> | <a href="?page=<?php echo $page - 1; ?>&limit=<?php echo $limit; ?>">Previous page</a>
+        <?php endif; ?>
+        Currently on page <?php echo $page; ?> of <?php echo $pages; ?>, Displaying <?php echo $start; ?>-<?php echo $end; ?> of <?php echo $studentcount; ?> results
+        <?php if ($page < $pages) : ?>
+            <a href="?page=<?php echo $page + 1; ?>&limit=<?php echo $limit; ?>" title="Next page">Next page</a>
+            <a href="?page=<?php echo $pages; ?>&limit=<?php echo $limit; ?>" title="Last Page">Last page</a>
+        <?php endif; ?>
         <table>
             <tr>
                 <th>ID</th>
@@ -47,12 +72,11 @@ $results = GetAllStudents($conn, $start, $end);
                 </tr>
             <?php endforeach; ?>
         </table>
-        <a href="index.php?page=<?php echo $nextpage; ?>">Next</a>
+
     <?php else : ?>
-        <table>
-            <tr>
-                <td>No students to show</td>
-            </tr>
+        <tr>
+            <td>No students to show</td>
+        </tr>
         </table>
     <?php endif; ?>
 </body>
